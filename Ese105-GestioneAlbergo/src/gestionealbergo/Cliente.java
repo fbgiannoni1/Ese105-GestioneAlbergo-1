703d1fc34770c71339d24804e5392e78f2dc434e
Federico <federicoalunni97@libero.it>
2015-04-22 12:52:27 +0200

    Update Cliente.java

--- Ese105-GestioneAlbergo/src/gestionealbergo/Cliente.java
+++ Ese105-GestioneAlbergo/src/gestionealbergo/Cliente.java
@@ -1,5 +1,60 @@
 package gestionealbergo;
 
-public class Cliente {
+	public class Cliente 
+	{
+		 private String nome;
+		 private String cognome;
+		 private String cellulare;
+		 private String email;
+		 
+		 public Cliente(String nome, String cognome, String cellulare)
+		 {
+			super();
+			this.nome = nome;
+			this.cognome = cognome;
+			this.cellulare = cellulare;
+	   	}
 
+		public String getNome()
+		{
+			return nome;
+		}
+
+		public void setNome(String nome) 
+		{
+			this.nome = nome;
+		}
+
+		public String getCognome() 
+		{
+			return cognome;
+		}
+
+		public void setCognome(String cognome)
+		{
+			this.cognome = cognome;
+		}
+
+		public String getCellulare() 
+		{
+			return cellulare;
+		}
+
+		public void setCellulare(String cellulare) 
+		{
+			this.cellulare = cellulare;
+		}
+
+		public String getEmail() 
+		{
+			return email;
+		}
+
+		public void setEmail(String email)
+		{
+			this.email = email;
+		}
+		
+		 
 }
+
