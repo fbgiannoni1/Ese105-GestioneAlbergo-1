642f53e01a9e0c3d3de5bc69557a4b949d08a09d
Federico <federicoalunni97@libero.it>
2015-04-22 12:51:50 +0200

    Update Camera.java

--- Ese105-GestioneAlbergo/src/gestionealbergo/Camera.java
+++ Ese105-GestioneAlbergo/src/gestionealbergo/Camera.java
@@ -1,6 +1,35 @@
 package gestionealbergo;
 
-public class Camera
+import java.time.*;
+ 
+abstract public class Camera
 {
+	private String numero;	
+	
+	public Camera(String numero) 
+	{
+		super();
+		this.numero = numero;
+	}
 
+	public String getNumero() 
+	{
+		return numero;
+	}
+
+	public void setNumero(String numero) 
+	{
+		this.numero = numero;
+	}
+	
+	public void addPrenotazione(Prenotazione p)
+	{
+		
+	}
+	
+	public boolean isDisponibile(LocalDataTime dal, LocalDataTime al)
+	{
+	
+	}
+	
 }
